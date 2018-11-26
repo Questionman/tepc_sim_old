e716b6fe4dbafbc219c021e5d8d8d08c816d2637
Jihong Jung <diti94@naver.com>
2018-11-26 06:35:51 +0000

    edit macro regarding source size with Detector settings

--- src/B1DetectorConstruction.cc
+++ src/B1DetectorConstruction.cc
@@ -156,14 +156,14 @@ G4VPhysicalVolume* B1DetectorConstruction::Construct()
   // Regolith
   
   G4double regolith_depth = 0.99*0.25*m;
-  G4Box* solidRegolith = new G4Box("Regolith", 0.499*world_size, 0.499*world_size, 0.5*regolith_depth);
+  G4Box* solidRegolith = new G4Box("Regolith", 0.4999*world_size, 0.4999*world_size, 0.5*regolith_depth);
   G4LogicalVolume* logicRegolith = new G4LogicalVolume(solidRegolith, regolith_mat, "Regolith");
   logicRegolith->SetUserLimits(new G4UserLimits(10*cm));
   new G4PVPlacement(0,G4ThreeVector(0,0, -0.5*regolith_depth), logicRegolith, "Regolith", logicWorld, false,5, true);
   
   //------------------------------------------------------
   //Atmosphere
-  G4Sphere* solidAtmosphere = new G4Sphere("Atmosphere", 6.980001*cm, 0.499*world_size, 0, 360*degree, 0, 90*degree);
+  G4Sphere* solidAtmosphere = new G4Sphere("Atmosphere", 6.980001*cm, 0.4999*world_size, 0, 360*degree, 0, 90*degree);
   G4LogicalVolume* logicAtmosphere = new G4LogicalVolume(solidAtmosphere,atmosphere_mat, "Atmosphere");
   logicAtmosphere->SetUserLimits(new G4UserLimits(10*cm));
   new G4PVPlacement(0, G4ThreeVector(0,0,0.00001*cm), logicAtmosphere,"Atmosphere",logicWorld,false,6, true);
---
+++ src/B1RunData.cc
@@ -0,0 +1,79 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+// $Id: B1RunData.cc 100946 2016-11-03 11:28:08Z gcosmo $
+//
+/// \file B1RunData.cc
+/// \brief Implementation of the B1RunData class
+
+#include "B1RunData.hh"
+#include "B1Analysis.hh"
+
+#include "G4RunManager.hh"
+#include "G4UnitsTable.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1RunData::B1RunData() 
+ : G4Run(),
+   fVolumeNames{ { "InnerPropane"} }
+{
+  for ( auto& edep : fEdep ) { 
+    edep = 0.; 
+  }
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+B1RunData::~B1RunData()
+{;}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1RunData::FillPerEvent()
+{
+  // get analysis manager
+  auto analysisManager = G4AnalysisManager::Instance();
+
+  // accumulate statistic
+  // in the order od the histograms, ntuple columns declarations
+  G4int counter = 0;
+  for ( auto edep : fEdep ) {
+    analysisManager->FillH1(counter, edep);
+    analysisManager->FillNtupleDColumn(counter++, edep);
+  }
+  analysisManager->AddNtupleRow();  
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void B1RunData::Reset()
+{ 
+  for ( auto& edep : fEdep ) { 
+    edep = 0.; 
+  }
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
